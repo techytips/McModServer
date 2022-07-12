da2b6cf095fade30f04ad75714c0a4f20f66420b
techy <techy@gmail.com>
2022-07-11 22:44:40 -0700

    first commit

---
+++ src/ca/obsidiantech/mcmod/Copy.java
@@ -0,0 +1,172 @@
+package ca.obsidiantech.mcmod;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import net.lingala.zip4j.ZipFile;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+
+public class Copy extends Thread {
+  public static String pluginpath = "plugins/McModServer";
+  public static String mojangAPI = "https://api.minecraftservices.com/minecraft/profile/lookup/name/";
+  public static String webpath = "";
+  public static String downpath = "";
+  public static CommandSender person = null;
+  @Override
+  public void run() {
+    try {
+      FileFilter filter = new FileFilter() {
+        public boolean accept(File file) {
+          if (file.getName().endsWith(".lock")) {
+            return false;
+          }
+          return true;
+        }
+      };
+      //copying all the worlds
+      File worldSrc = new File("world");
+      File worldDest = new File(pluginpath + "/world");
+      FileUtils.copyDirectory(worldSrc, worldDest, filter);
+      person.sendMessage("Copying.(1/6)");
+
+      File netherSrc = new File("world_nether/DIM-1");
+      File netherDest = new File(pluginpath + "/world/DIM-1");
+      FileUtils.copyDirectory(netherSrc, netherDest, filter);
+      person.sendMessage("Copying..(2/6)");
+
+      File endSrc = new File("world_the_end/DIM1");
+      File endDest = new File(pluginpath + "/world/DIM1");
+      FileUtils.copyDirectory(endSrc, endDest, filter);
+      person.sendMessage("Copying...(3/6)");
+
+      if (Bukkit.getOnlineMode() == false) {
+        convertUsers(pluginpath);
+        person.sendMessage("Processing.(4/6)");
+      }
+    } catch (Exception e) {
+      Bukkit.getLogger().warning("Error Copying files (or processing player data)");
+    }
+    try {
+    	String randtemp = generateRand();
+    	person.sendMessage("Zipping..(5/6)");
+	    new ZipFile(pluginpath + "/world.zip").addFolder(new File(pluginpath + "/world"));
+	    person.sendMessage("Finishing up...(6/6)");
+	    FileUtils.moveFile(new File(pluginpath + "/world.zip"), new File(webpath + randtemp + "/Techy's-Server.zip"));
+	    Main.lastWD = webpath + randtemp + "/Techy's-Server.zip";
+	      
+	    person.sendMessage("Here's your world download:");
+	    sendLink(downpath + randtemp + "/Techy's-Server.zip");
+	      
+	    cleanDownloads();
+	    File Dest = new File(pluginpath + "/world");
+	    FileUtils.deleteDirectory(Dest);
+	    Main.inProgress = false;
+	
+	    } catch (Exception e1) {
+	      Bukkit.getLogger().warning("error");
+	    }
+
+  }
+  public static void cleanDownloads() {
+	  //enumerates through all the folders in the world downloads folder 
+	  //and deletes them if they are older than an hour from now
+	  File dir = new File(webpath);
+      String names[] = dir.list();
+      for (int x = 0; x < names.length; x++) {
+    	  File del = new File(webpath + names[x]);
+    	  Date hourago = new Date(System.currentTimeMillis() - 3600 * 1000);
+    	  if (FileUtils.isFileOlder(del, hourago)) {
+    		  try {
+    			  del.delete();
+    			  Bukkit.getLogger().info("Deleted file (" + names[x] + ") in webpath");
+    		  }catch(Exception delerror) {
+    			  Bukkit.getLogger().warning("Encountered an error deleting files in the the webpath");
+    		  }
+    		  
+    	  }
+          
+
+        
+      }
+  }
+  public static void sendLink(String link) {
+    TextComponent t = new TextComponent();
+    t.setText(link);
+    t.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, link));
+    t.setColor(ChatColor.GREEN);
+    t.setUnderlined(true);
+    person.spigot().sendMessage(t);
+  }
+
+  public static void convertUsers(String path) {
+    OfflinePlayer[] players = Bukkit.getOfflinePlayers();
+    for (int y = 0; y < players.length; y++) {
+      File datSrc = new File(path + "/world/playerdata/" + getServerUUID(players[y]) + ".dat");
+      String onlinename = getUUID(players[y].getName());
+      File datDest = new File(path + "/world/playerdata/" + onlinename + ".dat");
+      try {
+        if (onlinename == "") {} else {
+          FileUtils.copyFile(datSrc, datDest);
+        }
+      } catch (Exception e) {
+        Bukkit.getLogger().warning("error at player");
+      }
+    }
+  }
+  public static String getUUID(String user) {
+    try {
+      StringBuilder output = new StringBuilder();
+      URL api = new URL(mojangAPI + user);
+      HttpURLConnection con = (HttpURLConnection) api.openConnection();
+      con.setRequestMethod("GET");
+      if (con.getResponseCode() != 200) {
+        return "";
+      }
+      try (BufferedReader reader = new BufferedReader(
+        new InputStreamReader(con.getInputStream()))) {
+        for (String line;
+          (line = reader.readLine()) != null;) {
+          output.append(line);
+        }
+      }
+      JsonObject jsonObject = new JsonParser().parse(output.toString()).getAsJsonObject();
+      UUID uuid = java.util.UUID.fromString(jsonObject.get("id").getAsString().replaceFirst(
+        "(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)", "$1-$2-$3-$4-$5"
+      ));
+      return uuid.toString();
+
+    } catch (Exception e) {
+      Bukkit.getLogger().warning("Error with mojang api, please wait 10 minutes.");
+      return null;
+    }
+  }
+  public static String getServerUUID(OfflinePlayer user) {
+    return user.getUniqueId().toString();
+  }
+  public static String generateRand() {
+    String chars = "ABCDEFGHIJKLMNOPQRSTUVQXYZ1234567890";
+    String random = "";
+    for (int x = 0; x < 10; x++) {
+      random += chars.charAt((int)(Math.random() * 36));
+    }
+    return random;
+  }
+
+}
---
+++ src/ca/obsidiantech/mcmod/Main.java
@@ -0,0 +1,62 @@
+package ca.obsidiantech.mcmod;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin {
+	public static boolean inProgress= false;
+	public static String webpath = "/webpath/worlds/";
+	public static String downpath = "https://files.obsidiantech.ca/worlds/";
+	public static String lastWD = null;
+    public void onEnable() {
+		getDataFolder().mkdirs();
+		Copy.webpath = webpath;
+		Copy.downpath = downpath;
+	}
+    @Override
+    public void onDisable() {}
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("modadmin")) {
+        	if (inProgress == true) {
+        		sender.sendMessage("Error, world download in progress");
+        	}else {
+            	
+            	
+        	}
+        	return true;
+        }
+        
+        if (command.getName().equalsIgnoreCase("getworld")) {
+        	if (lastWD == null || ) {
+        		sender.sendMessage("No recent download found");
+        		sender.sendMessage("Executing world download");
+        		if (inProgress == true) {
+            		sender.sendMessage("World download in progress, please wait a bit.");
+            	}else{
+            		inProgress = true;
+                	Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "save-all");        	
+                	Copy threadcopy = new Copy();
+                	threadcopy.person = sender;
+                	threadcopy.start();
+            	}
+        	}else {
+        		sender.sendMessage("Recent world download found");
+        		Copy.person = sender;
+        		Copy.sendLink(lastWD);
+        	}
+        	
+        	
+        	return true;
+        }
+        return false;    
+    }
+    
+    
+    
+    
+    
+    
+}
